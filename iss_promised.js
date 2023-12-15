95eed93405a94c978f43aac56943246e58215ed1
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-15 11:28:45 -0500

    ISS spotter with Promises

---
+++ iss_promised.js
@@ -0,0 +1,34 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const parsedBody = JSON.parse(body);
+  const latitude = parsedBody.latitude;
+  const longitude = parsedBody.longitude;
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(body => {
+      const parsedBody = JSON.parse(body);
+      const passTimes = parsedBody.response;
+      return passTimes;
+    });
+};
+
+
+
+
+module.exports = { nextISSTimesForMyLocation };
